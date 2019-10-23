bc0c1321a74cd9322cf39ab7423f6faa563ad622
Georgios Andrianakis <geoand@gmail.com>
2019-10-23 15:45:57 +0300

    Fix minikube scripts

--- hello/quarked/knburst_minikube.sh
+++ hello/quarked/knburst_minikube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 INGRESSGATEWAY=istio-ingressgateway
-IP_ADDRESS="$(minikube -p knative ip):$(kubectl get svc $INGRESSGATEWAY --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
+IP_ADDRESS="$(minikube ip):$(kubectl get svc istio-ingressgateway --namespace istio-system --output 'jsonpath={.spec.ports[?(@.port==80)].nodePort}')"
 HOST_URL=$(kubectl get routes.serving.knative.dev quarked  -o jsonpath='{.status.url}')
 STRIPPED=$(echo $HOST_URL | cut -f2 -d':' | cut -f3 -d'/')
 
