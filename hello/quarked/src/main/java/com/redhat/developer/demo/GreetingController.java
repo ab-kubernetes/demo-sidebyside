2bbbad13903e76064deaa27a8786a4204d976a97
Georgios Andrianakis <geoand@gmail.com>
2019-10-30 17:16:08 +0200

    Simplify Spring controller in Quarkus

--- hello/quarked/src/main/java/com/redhat/developer/demo/GreetingController.java
+++ hello/quarked/src/main/java/com/redhat/developer/demo/GreetingController.java
@@ -2,12 +2,10 @@ package com.redhat.developer.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/")
 public class GreetingController {
     
     private String prefix = "Yo";
