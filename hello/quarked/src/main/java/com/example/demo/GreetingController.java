140c91545bb0bdd0c78cddffb60e75dd42cd3845
Charles Moulliard <cmoulliard@redhat.com>
2020-01-03 16:25:32 +0100

    chore: Use same package name as spring boot hellp example

--- hello/quarked/src/main/java/com/example/demo/GreetingController.java
+++ hello/quarked/src/main/java/com/example/demo/GreetingController.java
@@ -1,9 +1,7 @@
-package com.redhat.developer.demo;
-
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 public class GreetingController {
